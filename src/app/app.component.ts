0dd9904cb972b4d11887fc33da53d9e5a2f0a339
jerecede <jeremias10458@gmail.com>
2025-04-16 11:15:22 +0200

    creazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,32 @@ import { ProcessingService } from './services/processing/processing.service';
 })
 export class AppComponent {
   title = 'temp-nan';
+  data = null;
+  tempMean: number = 0;
+  maxTempTime: TempTime = {time: '', temp: 0};
+  minTempTime: TempTime = {time: '', temp: 0};
+  startDate = '';
+  endDate = '';
 
   dataServ = inject(DataService);
-  processingServ = inject(ProcessingService)
+  processingServ = inject(ProcessingService);
+
+  round = Math.round;
 
   ngOnInit(){
     this.transformData()
   }
 
   async transformData(){
-    const data = await this.dataServ.getData()
-    const tempArray: TempTime[] = this.processingServ.getTempArrayFromHourlyData(data.hourly);
-    const maxTempTime: TempTime = this.processingServ.getMaxTemp(tempArray);
-    const minTempTime: TempTime = this.processingServ.getMinTemp(tempArray);
-    const tempMean: number = this.processingServ.getTempMean(tempArray);
+    const data = await this.dataServ.getData();
+    console.log(data);
+    const tempArray = this.processingServ.getTempArrayFromHourlyData(data.hourly);
+    this.maxTempTime = this.processingServ.getMaxTemp(tempArray);
+    this.minTempTime = this.processingServ.getMinTemp(tempArray);
+    this.tempMean = this.processingServ.getTempMean(tempArray);
+
+    this.startDate = tempArray[0].time;
+    this.endDate = tempArray[tempArray.length-1].time;
   }
 
 }
--- src/app/services/processing/processing.service.ts
+++ src/app/services/processing/processing.service.ts
@@ -10,21 +10,63 @@ export class ProcessingService {
   constructor() { }
 
   getTempArrayFromHourlyData(hourly: any): TempTime[] {
-    return []
+    const tempTimeArray: TempTime[] = [];
+    for (let i = 0; i < hourly.temperature_2m.length; i++) {
+      const tempCurr = hourly.temperature_2m[i];
+      const timeCurr = hourly.time[i];
+      const tempTimeCUrr: TempTime = {
+        time: timeCurr,
+        temp: this.fromFtoC(tempCurr),
+      }
+      tempTimeArray.push(tempTimeCUrr);
+    }
+    return tempTimeArray;
   }
 
   getMinTemp(tempArray: TempTime[]): TempTime {
-    throw new Error('Method not implemented.');
+    let minTemp = Infinity;
+    let indexMinTemp = -1;
+    let i = 0;
+
+    for (i; i < tempArray.length; i++) {
+      const tempTime = tempArray[i];
+      const temp = tempTime.temp;
+      if(temp < minTemp){
+        minTemp = temp;
+        indexMinTemp = i;
+      }
+    }
+    return tempArray[indexMinTemp];
   }
+
   getTempMean(tempArray: TempTime[]): number {
-    throw new Error('Method not implemented.');
+    let tot = 0;
+    for (let i = 0 ; i < tempArray.length; i++) {
+      const tempTime = tempArray[i];
+      tot += tempTime.temp;
+    }
+    const mean = tot / tempArray.length;
+    return mean;
   }
+
   getMaxTemp(tempArray: TempTime[]): TempTime {
-    throw new Error('Method not implemented.');
+    let maxTemp = -Infinity;
+    let indexMaxTemp = -1;
+    let i = 0;
+
+    for (i; i < tempArray.length; i++) {
+      const tempTime = tempArray[i];
+      const temp = tempTime.temp;
+      if(temp > maxTemp){
+        maxTemp = temp;
+        indexMaxTemp = i;
+      }
+    }
+    return tempArray[indexMaxTemp];
   }
 
   fromFtoC(f: number): number{
-    return -1;
+    return (f-32)*(5/9);
   }
 
 }
